f98264f862448da036ed3e463418d59792e29da1
Venukumar9 <107048250+Venukumar9@users.noreply.github.com>
2022-08-08 22:34:08 +0530

    StreamAPI Example Programs

---
+++ StreamAPI/src/com/motivity/Employee.java
@@ -0,0 +1,11 @@
+package com.motivity;
+
+public class Employee {
+    String name;
+    double sal;
+
+    public Employee(String name, double sal) {
+        this.name = name;
+        this.sal = sal;
+    }
+}
